fd1e78f43fbf5b32513817adeca081fc0a833d13
abigsam <abigsam@gmail.com>
2021-09-26 14:18:18 +0300

    Add nixie display empty class

---
+++ lib/nixie_display/nixie_display.cpp
@@ -0,0 +1,10 @@
+#include "nixie_display.h"
+
+
+nixie_display::nixie_display(/* args */)
+{
+}
+
+nixie_display::~nixie_display()
+{
+}
---
+++ lib/nixie_display/nixie_display.h
@@ -0,0 +1,18 @@
+#ifndef __NIXIE_DISPLAY_H
+#define __NIXIE_DISPLAY_H
+
+#include <Arduino.h>
+#include <SPI.h>
+// #include "nixie_clock_defines.h"
+
+class nixie_display
+{
+private:
+    /* data */
+public:
+    nixie_display(/* args */);
+    ~nixie_display();
+};
+
+
+#endif //__NIXIE_DISPLAY_H
