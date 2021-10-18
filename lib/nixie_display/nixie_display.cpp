8a60d8817fd1d4287beffc37258b273879bd9628
abigsam <abigsam@gmail.com>
2021-10-18 21:50:55 +0300

    Add nixie display plug

--- lib/nixie_display/nixie_display.cpp
+++ lib/nixie_display/nixie_display.cpp
@@ -3,8 +3,24 @@
 
 nixie_display::nixie_display(/* args */)
 {
+    SPI.begin();
 }
 
 nixie_display::~nixie_display()
 {
+}
+
+// void nixie_display::init()
+// {
+
+// }
+
+void nixie_display::clear()
+{
+    
+}
+
+void nixie_display::display(time_data *curr_time, uint8_t point_mask)
+{
+
 }
--- lib/nixie_display/nixie_display.h
+++ lib/nixie_display/nixie_display.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include <SPI.h>
+#include "time_data.h"
 // #include "nixie_clock_defines.h"
 
 class nixie_display
@@ -12,7 +13,12 @@ private:
 public:
     nixie_display(/* args */);
     ~nixie_display();
+    // void init();
+    void clear();
+    void display(time_data *curr_time, uint8_t point_mask);
 };
+
+
 
 
 #endif //__NIXIE_DISPLAY_H
