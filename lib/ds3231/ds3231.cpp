14e1bb072dafbd9b05dc7d0bbff77b0f809a4c41
abigsam <abigsam@gmail.com>
2021-10-18 21:49:43 +0300

    Add read_rtc method

--- lib/ds3231/ds3231.cpp
+++ lib/ds3231/ds3231.cpp
@@ -1,5 +1,9 @@
 #include "ds3231.h"
 
+#define GET_ONES(d)             ((d) & (uint8_t)0x0f )
+#define GET_TENS(d)             (((d) >> 4) & (uint8_t)0x0f )
+
+
 ds3231::ds3231(/* args */)
 {
     _last_error = 0u;
@@ -65,13 +69,60 @@ void ds3231::write_arr(uint8_t start_addr, uint8_t regs_num, uint8_t *in_arr)
 }
 
 
-void ds3231::update_rtc(time_data * new_time)
+void ds3231::update_rtc(const time_data * new_time)
 {
-    
+    uint8_t warr[(DS3231_YEAR_REG - DS3231_SECONDS_REG)] = {0u};
+
+
+    //combine
+
+    // warr[DS3231_SECONDS_REG - DS3231_SECONDS_REG] = new_time->seconds.combine();
+
+    uint8_t t = new_time->seconds.
+
+
+    write_arr(DS3231_SECONDS_REG, (DS3231_YEAR_REG - DS3231_SECONDS_REG), warr);
 }
 
 
-void ds3231::read_rtc(time_data * read_time)
+void ds3231::read_rtc(time_data *read_time)
 {
+    uint8_t rarr[(DS3231_YEAR_REG - DS3231_SECONDS_REG)] = {0u};
+    read_arr(DS3231_SECONDS_REG, (DS3231_YEAR_REG - DS3231_SECONDS_REG), rarr);
+    if (_last_error) {
+        //Error occured
+        return;
+    }
+    //Seconds & minutes
+    read_time->seconds.ones = GET_ONES( rarr[DS3231_SECONDS_REG - DS3231_SECONDS_REG] );
+    read_time->seconds.tens = GET_TENS( rarr[DS3231_SECONDS_REG - DS3231_SECONDS_REG] );
+    read_time->minutes.ones = GET_ONES( rarr[DS3231_MINUTES_REG - DS3231_SECONDS_REG] );
+    read_time->minutes.tens = GET_TENS( rarr[DS3231_MINUTES_REG - DS3231_SECONDS_REG] );
+    //Hours
+    if (DS3231_HOURS_12H_MODE & rarr[DS3231_HOURS_REG - DS3231_SECONDS_REG]) {
+        //12h mode
+        uint8_t h = GET_ONES( rarr[DS3231_HOURS_REG - DS3231_SECONDS_REG] );
+        if (DS3231_HOURS_10H_MASK & rarr[DS3231_HOURS_REG - DS3231_SECONDS_REG]) {
+            h += 10u;
+        }
+        if (DS3231_HOURS_AMPM_MASK & rarr[DS3231_HOURS_REG - DS3231_SECONDS_REG]) {
+            h += 12u;
+        }
+        read_time->hours.init(h);
+    } else {
+        //24h mode
+        read_time->hours.ones = GET_ONES( rarr[DS3231_HOURS_REG - DS3231_SECONDS_REG] );
+        read_time->hours.tens = GET_TENS( rarr[DS3231_HOURS_REG - DS3231_SECONDS_REG] );
+    }
+    //Day (skip)
+    //Date
+    read_time->date.ones = GET_ONES( rarr[DS3231_DATE_REG - DS3231_SECONDS_REG] );
+    read_time->date.tens = GET_TENS( rarr[DS3231_DATE_REG - DS3231_SECONDS_REG] );
+    //Month (eleminate century bits)
+    read_time->month.ones = GET_ONES( rarr[DS3231_MONTH_REG - DS3231_SECONDS_REG] );
+    read_time->month.tens = ~(DS3231_MONTH_CENTURY_MASK >> 4) & GET_TENS( rarr[DS3231_MONTH_REG - DS3231_SECONDS_REG] );
+    //Year
+    read_time->year.ones = GET_ONES( rarr[DS3231_YEAR_REG - DS3231_SECONDS_REG] );
+    read_time->year.tens = GET_TENS( rarr[DS3231_YEAR_REG - DS3231_SECONDS_REG] );
 
 }
--- lib/ds3231/ds3231.h
+++ lib/ds3231/ds3231.h
@@ -9,6 +9,18 @@
 #define DS3231_ADDRESS              ((uint8_t) 0x68)
 #define DS3231_REG_CONTROL          ((uint8_t) 0x0e)
 #define DS3231_CONTROL_DEFAULT      ((uint8_t) 0x1c)
+//
+#define DS3231_SECONDS_REG          ((uint8_t) 0x00)
+#define DS3231_MINUTES_REG          ((uint8_t) 0x01)
+#define DS3231_HOURS_REG            ((uint8_t) 0x02)
+#define DS3231_HOURS_12H_MODE       ((uint8_t) 0x40)
+#define DS3231_HOURS_10H_MASK       ((uint8_t) 0x10)
+#define DS3231_HOURS_AMPM_MASK      ((uint8_t) 0x20)
+#define DS3231_DAY_REG              ((uint8_t) 0x03)
+#define DS3231_DATE_REG             ((uint8_t) 0x04)
+#define DS3231_MONTH_REG            ((uint8_t) 0x05)
+#define DS3231_MONTH_CENTURY_MASK   ((uint8_t) 0x80)
+#define DS3231_YEAR_REG             ((uint8_t) 0x06)
 
 
 class ds3231
@@ -27,7 +39,7 @@ public:
     ds3231(/* args */);
     ~ds3231();
 
-    void update_rtc(time_data * new_time);
+    void update_rtc(const time_data * new_time);
     void read_rtc(time_data * read_time);
 };
 
