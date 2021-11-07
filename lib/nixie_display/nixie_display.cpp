2548c06804b7c0d998936f9ee450f35e8bb37a29
abigsam <abigsam@gmail.com>
2021-11-07 19:43:56 +0200

    Update nixie display library with desired methods

--- lib/nixie_display/nixie_display.cpp
+++ lib/nixie_display/nixie_display.cpp
@@ -5,6 +5,10 @@
 #define BEGIN_TRANSACTION()     do {SPI.beginTransaction(SPISettings(SPI_CLOCK_HZ, MSBFIRST, SPI_MODE0));} while(0);
 #define GET_REG_NUM(n)          ((n) / 8u)
 #define GET_REG_MASK(n)         ((uint8_t)(1u << ((n) % 8u)))
+#define MINUTES_ONES_DIGIT      (0u)
+#define MINUTES_TENS_DIGIT      (1u)
+#define HOURS_ONES_DIGIT        (2u)
+#define HOURS_TENS_DIGIT        (3u)
 
 
 const PROGMEM uint8_t MINUTES_ONES_SET[] = {
@@ -64,6 +68,10 @@ const PROGMEM uint8_t HOURS_TENS_SET[] = {
 };
 
 
+/**
+ * @brief Nixie display class constructor
+ * 
+ */
 nixie_display::nixie_display(/* args */)
 {
     SPI.begin();
@@ -72,6 +80,23 @@ nixie_display::nixie_display(/* args */)
     digitalWrite(SHIFT_REG_LOAD_PIN, LOW);
 }
 
+/**
+ * @brief Send update strobe to the external shift register
+ * 
+ */
+void nixie_display::update_shift_reg()
+{
+    digitalWrite(SHIFT_REG_LOAD_PIN, HIGH);
+    delay(UPD_DELAY_MS);
+    digitalWrite(SHIFT_REG_LOAD_PIN, LOW);
+}
+
+/**
+ * @brief Write display buffer to the external shift register
+ * 
+ * @param ptr   Pointer to the buffer
+ * @param size  Number of digits
+ */
 void nixie_display::write_display(uint8_t *ptr, uint8_t size)
 {
     SPI.beginTransaction(SPISettings(SPI_CLOCK_HZ, MSBFIRST, SPI_MODE0));
@@ -80,40 +105,94 @@ void nixie_display::write_display(uint8_t *ptr, uint8_t size)
     update_shift_reg();
 }
 
-void nixie_display::update_shift_reg()
+/**
+ * @brief Get convert table pointer in FLASH for specified display digit number
+ * 
+ * @param display_digit     Display digit number, should be in the range 0...3
+ * @return uint8_t*         Pointer to the convert table in the FLASH
+ */
+uint8_t* nixie_display::get_table_ptr(uint8_t display_digit)
 {
-    digitalWrite(SHIFT_REG_LOAD_PIN, HIGH);
-    delay(UPD_DELAY_MS);
-    digitalWrite(SHIFT_REG_LOAD_PIN, LOW);
+    if (3u == display_digit)
+        return (uint8_t *)HOURS_TENS_SET;
+    else if (2u == display_digit)
+        return (uint8_t *)HOURS_ONES_SET;
+    else if (1u == display_digit)
+        return (uint8_t *)MINUTES_TENS_SET;
+    else
+        return (uint8_t *)MINUTES_ONES_SET;
 }
 
-void nixie_display::set_shift_reg(const uint8_t * PMBCD_PTR, uint8_t bcd_num)
+/**
+ * @brief Clear display digit
+ * 
+ * @param digit_num     Display digit number that need to clear
+ * @return uint8_t*     Pointer to the convert table in the FLASH
+ */
+uint8_t* nixie_display::clr_display_buffer(uint8_t digit_num)
+{
+    uint8_t *ptr = get_table_ptr(digit_num);
+    uint8_t reg_num = 0u, mask = 0u;
+    for(auto i = 0u; i < 10u; i++) {
+        reg_num = pgm_read_byte_near(ptr + i*2u);
+        mask = pgm_read_byte_near(ptr + i*2u + 1u);
+        arr[reg_num] &= ~mask;
+    }
+    return ptr;
+}
+
+/**
+ * @brief Set display buffer with specified number
+ * 
+ * @param digit_num     Display digit number
+ * @param value         Char need to be displayed; can be in range '0'...'9' or ' ' for clear
+ * @return uint8_t*     Pointer to the convert table in the FLASH 
+ */
+uint8_t* nixie_display::set_display_buffer(uint8_t digit_num, char value)
 {
     uint8_t reg_num = 0u, mask = 0u;
-    reg_num = pgm_read_byte_near(PMBCD_PTR + bcd_num*2u);
-    mask = pgm_read_byte_near(PMBCD_PTR + bcd_num*2u + 1u);
-    arr[reg_num] = mask;
+    uint8_t *ptr = clr_display_buffer(digit_num);
+    if (value >= '0' || value <= '9') {
+        reg_num = pgm_read_byte_near(ptr + (value - '0')*2u);
+        mask = pgm_read_byte_near(ptr + (value - '0')*2u + 1u);
+        arr[reg_num] = mask;
+    }
+    return ptr;
 }
 
+
+/**
+ * @brief Decode DateTime class value and update display buffer
+ * 
+ * @param time  DateTime class with time value
+ */
 void nixie_display::decode_time(DateTime *time)
 {
     uint8_t ones = 0u, tens = 0u;
     //Clear buffer
-    memset(arr, (uint8_t)0u, REGISTERS_NUM);
+    // memset(arr, (uint8_t)0u, REGISTERS_NUM);
     //Minutes
     ones = time->minute() % 10u;
     tens = time->minute() / 10u;
-    set_shift_reg(MINUTES_ONES_SET, ones);
-    set_shift_reg(MINUTES_TENS_SET, tens);
+    set_display_buffer(MINUTES_ONES_DIGIT, (char)('0' + ones));
+    set_display_buffer(MINUTES_TENS_DIGIT, (char)('0' + tens));
     //Hours
     ones = time->hour() % 10u;
     tens = time->hour() / 10u;
-    set_shift_reg(HOURS_ONES_SET, ones);
-    if (tens > 0u && (uint8_t)HIDE_HOUR_TENS_IF_ZERO > 0u)
-        set_shift_reg(HOURS_TENS_SET, tens);
+    set_display_buffer(HOURS_ONES_DIGIT, (char)('0' + ones));
+    if (0u == tens && (uint8_t)HIDE_HOUR_TENS_IF_ZERO > 0u)
+        set_display_buffer(HOURS_TENS_DIGIT, ' ');
+    else
+        set_display_buffer(HOURS_TENS_DIGIT, (char)('0' + tens));
 }
 
-void nixie_display::decode_points(decimal_pnt_t point_mask)
+
+/**
+ * @brief Control decimal points and update display buffer
+ * 
+ * @param point_mask    Decimap points mask; can be one/ORed value POINT_OFF, POINT_MINUTE_TENS, POINT_HOUR_ONES, POINT_DONT_CHANGE
+ */
+void nixie_display::set_decimal_points(decimal_pnt_t point_mask)
 {
     uint8_t reg_num = 0u, mask = 0u;
 
@@ -137,12 +216,10 @@ void nixie_display::decode_points(decimal_pnt_t point_mask)
     }
 }
 
-void nixie_display::display_points(decimal_pnt_t point_mask)
-{
-    decode_points(point_mask);
-    write_display(arr, REGISTERS_NUM);
-}
-
+/**
+ * @brief Clear external shift register. Display buffer will not cleared
+ * 
+ */
 void nixie_display::clear()
 {
     BEGIN_TRANSACTION();
@@ -153,10 +230,45 @@ void nixie_display::clear()
     update_shift_reg();
 }
 
+/**
+ * @brief Display time
+ * 
+ * @param curr_time     Time value that need to be displayed
+ * @param point_mask    Decimap points mask; can be one/ORed value POINT_OFF, POINT_MINUTE_TENS, POINT_HOUR_ONES, POINT_DONT_CHANGE
+ */
 void nixie_display::display(DateTime *curr_time, uint8_t point_mask)
 {
     decode_time(curr_time);
-    if (!(POINT_DONT_CHANGE & point_mask))
-        decode_points((decimal_pnt_t)point_mask);
+    set_decimal_points((decimal_pnt_t)point_mask);
+    write_display(arr, REGISTERS_NUM);
+}
+
+/**
+ * @brief Set char to display. Updates only display beffer
+ * 
+ * @param val           Char to display; can be in range '0'...'9' or ' ' for clear sigit
+ * @param digit_num     Display digit number, can be in range 0...3
+ */
+void nixie_display::set_char(uint8_t digit_num, char val)
+{   
+    set_display_buffer(digit_num, val);
+}
+
+/**
+ * @brief Control decimal points and update display buffer
+ * 
+ * @param point_mask    Decimap points mask; can be one/ORed value POINT_OFF, POINT_MINUTE_TENS, POINT_HOUR_ONES, POINT_DONT_CHANGE
+ */
+void nixie_display::set_point(decimal_pnt_t point_mask)
+{
+    set_decimal_points((decimal_pnt_t)point_mask);
+}
+
+/**
+ * @brief Write buffer to the external shift register
+ * 
+ */
+void nixie_display::display_update()
+{
     write_display(arr, REGISTERS_NUM);
 }
--- lib/nixie_display/nixie_display.h
+++ lib/nixie_display/nixie_display.h
@@ -4,31 +4,40 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "RTClib.h"
-#include "nixie_clock_defines.h"
+#include "../../include/nixie_clock_defines.h"
 
 #define SPI_CLOCK_HZ                ((uint32_t) 100000)
 #define HIDE_HOUR_TENS_IF_ZERO      (1)
 
 typedef enum {
-    POINT_DONT_CHANGE = (1u << 2),
+    POINT_OFF         = 0u,
     POINT_MINUTE_TENS = (1u << 0),
-    POINT_HOUR_ONES   = (1u << 1)
+    POINT_HOUR_ONES   = (1u << 1),
+    POINT_DONT_CHANGE = (1u << 2)
 } decimal_pnt_t;
 
 class nixie_display
 {
 private:
-    void write_display(uint8_t *ptr, uint8_t size);
     void update_shift_reg();
-    void decode_time(DateTime *time);
-    void decode_points(decimal_pnt_t point_mask);
+    void write_display(uint8_t *ptr, uint8_t size);
     //
-    void set_shift_reg(const uint8_t * PMBCD_PTR, uint8_t num);
+    uint8_t* get_table_ptr(uint8_t display_digit);
+    uint8_t* clr_display_buffer(uint8_t digit_num);
+    uint8_t* set_display_buffer(uint8_t digit_num, char value);
+    //
+    void decode_time(DateTime *time);
+    void set_decimal_points(decimal_pnt_t point_mask);
 public:
     nixie_display(/* args */);
     void clear();
+    //Write time
     void display(DateTime *curr_time, uint8_t point_mask = POINT_DONT_CHANGE);
-    void display_points(decimal_pnt_t point_mask);
+    //Write any digit
+    void set_char(uint8_t digit_num, char val);
+    void set_point(decimal_pnt_t point_mask);
+    void display_update();
+    
 private:
     uint8_t arr [REGISTERS_NUM];
 };
