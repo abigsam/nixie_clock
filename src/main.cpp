9ed7758aa44cc13b188454b2878f9afa8fba960d
abigsam <abigsam@gmail.com>
2021-11-07 23:49:56 +0200

    Add change time mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include "nixie_clock_bsp.h"
 
+#define ENTER_MODE          (20u)
+
 DateTime curr_time;
 
 uint8_t update_time = 0u;
+uint16_t cnt_mode = 0u;
 
 void onAlarm();
 
@@ -11,9 +14,10 @@ void setup () {
     
     
     bsp_init();
+    bsp_check_display();
 
     // Making it so, that the alarm will trigger an interrupt
-    pinMode(RTC_INTERRUPT_PIN, INPUT_PULLUP);
+    
     attachInterrupt(digitalPinToInterrupt(RTC_INTERRUPT_PIN), onAlarm, FALLING);
 
     update_time = 1u;
@@ -22,11 +26,26 @@ void setup () {
 void loop () {
 
     if (update_time > 0) {
-        bsp_get_current_time(&curr_time);
+        bsp_get_current_time(curr_time);
         bsp_display_time(&curr_time);
         update_time = 0u;
     }
-    // delay(3000);
+    
+    delay(100);
+
+    if (bsp_read_btn(BTN_MODE)) {
+        cnt_mode++;
+    }
+
+    if (cnt_mode > ENTER_MODE) {
+        cnt_mode = 0u;
+        bool is_changed = bsp_mode_set(curr_time);
+        if (is_changed) {
+            bsp_set_new_time(curr_time);
+        } else {
+            bsp_get_current_time(curr_time);
+        }
+    }
 }
 
 
