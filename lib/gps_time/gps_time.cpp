1df6f4b5286da39c3f44b707cbbd18223d395ce4
abigsam <abigsam@gmail.com>
2021-12-11 13:04:13 +0200

    Add test program for checking GPS

--- lib/gps_time/gps_time.cpp
+++ lib/gps_time/gps_time.cpp
@@ -1,6 +1,84 @@
 #include "gps_time.h"
+#include <SoftwareSerial.h>
+#include "../../include/nixie_clock_defines.h"
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial sserial(GPS_RX, GPS_TX);
 
 gps_time::gps_time(/* args */)
 {
     
+}
+
+
+void gps_time::display_nfo()
+{
+    Serial.print(F("Location: ")); 
+    if (gps.location.isValid())
+    {
+        Serial.print(gps.location.lat(), 6);
+        Serial.print(F(","));
+        Serial.print(gps.location.lng(), 6);
+    }
+    else
+    {
+        Serial.print(F("INVALID"));
+    }
+
+    Serial.print(F("  Date/Time: "));
+    if (gps.date.isValid())
+    {
+        Serial.print(gps.date.month());
+        Serial.print(F("/"));
+        Serial.print(gps.date.day());
+        Serial.print(F("/"));
+        Serial.print(gps.date.year());
+    }
+    else
+    {
+        Serial.print(F("INVALID"));
+    }
+
+    Serial.print(F(" "));
+    if (gps.time.isValid())
+    {
+        if (gps.time.hour() < 10) Serial.print(F("0"));
+        Serial.print(gps.time.hour());
+        Serial.print(F(":"));
+        if (gps.time.minute() < 10) Serial.print(F("0"));
+        Serial.print(gps.time.minute());
+        Serial.print(F(":"));
+        if (gps.time.second() < 10) Serial.print(F("0"));
+        Serial.print(gps.time.second());
+        Serial.print(F("."));
+        if (gps.time.centisecond() < 10) Serial.print(F("0"));
+        Serial.print(gps.time.centisecond());
+    }
+    else
+    {
+        Serial.print(F("INVALID"));
+    }
+
+    Serial.println();
+}
+
+
+void gps_time::begin()
+{
+    digitalWrite(EN_GPS_PIN, LOW);
+    pinMode(EN_GPS_PIN, OUTPUT);
+    digitalWrite(EN_GPS_PIN, HIGH);
+    delay(400);
+    sserial.begin(GPS_UART_SPEED);
+}
+
+
+void gps_time::test()
+{
+    while (sserial.available() > 0)
+    if (gps.encode(sserial.read()))
+        display_nfo();
 }
--- lib/gps_time/gps_time.h
+++ lib/gps_time/gps_time.h
@@ -3,13 +3,17 @@
 
 #include <TinyGPS++.h>
 
+#define GPS_UART_SPEED          ((uint32_t) 9600)
 
 class gps_time
 {
 private:
     /* data */
+    void display_nfo();
 public:
     gps_time(/* args */);
+    void begin();
+    void test();
 };
 
 
