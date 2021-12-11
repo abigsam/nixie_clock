d12213902df705c143ae7a315e9689ca4b10731f
abigsam <abigsam@gmail.com>
2021-12-11 14:18:27 +0200

    Add date/time methods to the class

--- lib/gps_time/gps_time.cpp
+++ lib/gps_time/gps_time.cpp
@@ -1,27 +1,36 @@
 #include "gps_time.h"
-#include <SoftwareSerial.h>
+
 #include "../../include/nixie_clock_defines.h"
 
 // The TinyGPS++ object
-TinyGPSPlus gps;
+// TinyGPSPlus gps;
 
 // The serial connection to the GPS device
-SoftwareSerial sserial(GPS_RX, GPS_TX);
+// SoftwareSerial sserial(GPS_RX, GPS_TX);
 
+/**
+ * @brief gps_time class constructor
+ * 
+ */
 gps_time::gps_time(/* args */)
 {
-    
+    _gps = new TinyGPSPlus;
+    _sserial = new SoftwareSerial(GPS_RX, GPS_TX);
 }
 
 
-void gps_time::display_nfo()
+/**
+ * @brief Decode and display received data from GPS 
+ * 
+ */
+void gps_time::display_info()
 {
     Serial.print(F("Location: ")); 
-    if (gps.location.isValid())
+    if (_gps->location.isValid())
     {
-        Serial.print(gps.location.lat(), 6);
+        Serial.print(_gps->location.lat(), 6);
         Serial.print(F(","));
-        Serial.print(gps.location.lng(), 6);
+        Serial.print(_gps->location.lng(), 6);
     }
     else
     {
@@ -29,13 +38,13 @@ void gps_time::display_nfo()
     }
 
     Serial.print(F("  Date/Time: "));
-    if (gps.date.isValid())
+    if (_gps->date.isValid())
     {
-        Serial.print(gps.date.month());
+        Serial.print(_gps->date.month());
         Serial.print(F("/"));
-        Serial.print(gps.date.day());
+        Serial.print(_gps->date.day());
         Serial.print(F("/"));
-        Serial.print(gps.date.year());
+        Serial.print(_gps->date.year());
     }
     else
     {
@@ -43,19 +52,19 @@ void gps_time::display_nfo()
     }
 
     Serial.print(F(" "));
-    if (gps.time.isValid())
+    if (_gps->time.isValid())
     {
-        if (gps.time.hour() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.hour());
+        if (_gps->time.hour() < 10) Serial.print(F("0"));
+        Serial.print(_gps->time.hour());
         Serial.print(F(":"));
-        if (gps.time.minute() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.minute());
+        if (_gps->time.minute() < 10) Serial.print(F("0"));
+        Serial.print(_gps->time.minute());
         Serial.print(F(":"));
-        if (gps.time.second() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.second());
+        if (_gps->time.second() < 10) Serial.print(F("0"));
+        Serial.print(_gps->time.second());
         Serial.print(F("."));
-        if (gps.time.centisecond() < 10) Serial.print(F("0"));
-        Serial.print(gps.time.centisecond());
+        if (_gps->time.centisecond() < 10) Serial.print(F("0"));
+        Serial.print(_gps->time.centisecond());
     }
     else
     {
@@ -66,19 +75,174 @@ void gps_time::display_nfo()
 }
 
 
+/**
+ * @brief Makes necessary actions to run
+ * 
+ */
 void gps_time::begin()
 {
-    digitalWrite(EN_GPS_PIN, LOW);
+    power(false);
     pinMode(EN_GPS_PIN, OUTPUT);
-    digitalWrite(EN_GPS_PIN, HIGH);
+    power(true);
     delay(400);
-    sserial.begin(GPS_UART_SPEED);
+    _sserial->begin(GPS_UART_SPEED);
 }
 
 
+/**
+ * @brief Run test GPS
+ * 
+ */
 void gps_time::test()
 {
-    while (sserial.available() > 0)
-    if (gps.encode(sserial.read()))
-        display_nfo();
+    while (_sserial->available() > 0)
+    if (_gps->encode(_sserial->read())) {
+        display_info();
+    }
+}
+
+
+/**
+ * @brief Control GPS power
+ * 
+ * @param state     True for enable, False for disabling power
+ */
+void gps_time::power(bool state)
+{
+    digitalWrite(EN_GPS_PIN, (state) ? HIGH : LOW);
+}
+
+
+/**
+ * @brief 
+ * 
+ */
+void gps_time::read_gps()
+{
+    if (_sserial->available() > 0) {
+        _gps->encode(_sserial->read());
+    }
+}
+
+
+/**
+ * @brief Check if GPS location is avaliable
+ * 
+ * @return true     GPS location is avaliable/valid
+ * @return false    GPS location is not avaliable/not valid
+ */
+bool gps_time::location_valid()
+{
+    return _gps->location.isValid();
+}
+
+
+/**
+ * @brief Check if GPS time is avaliable
+ * 
+ * @return true     GPS time is avaliable/valid
+ * @return false    GPS time is not avaliable/not valid
+ */
+bool gps_time::time_valid()
+{
+    return (_gps->date.isValid() && _gps->time.isValid());
+}
+
+
+/**
+ * @brief Return latitude value
+ * 
+ * @return double   Latitude value
+ */
+double gps_time::get_lat()
+{
+    return (_gps->location.lat());
+}
+
+
+/**
+ * @brief Return longitude value
+ * 
+ * @return double   Longitude value
+ */
+double gps_time::get_lng()
+{
+    return (_gps->location.lng());
+}
+
+
+/**
+ * @brief Return GPS date month
+ * 
+ * @return uint8_t  Date month
+ */
+uint8_t gps_time::get_month()
+{
+    return (_gps->date.month());
+}
+
+
+/**
+ * @brief Return GPS date day
+ * 
+ * @return uint8_t  Date day
+ */
+uint8_t gps_time::get_day()
+{
+    return (_gps->date.day());
+}
+
+
+/**
+ * @brief Return GPS date year
+ * 
+ * @return uint16_t     Date year
+ */
+uint16_t gps_time::get_year()
+{
+    return (_gps->date.year());
+}
+
+
+/**
+ * @brief Return GPS time hour
+ * 
+ * @return uint8_t  Time hour
+ */
+uint8_t gps_time::get_hour()
+{
+    return (_gps->time.hour());
+}
+
+
+/**
+ * @brief Return GPS time minute
+ * 
+ * @return uint8_t  Time minute
+ */
+uint8_t gps_time::get_minute()
+{
+    return (_gps->time.minute());
+}
+
+
+/**
+ * @brief Return GPS time second
+ * 
+ * @return uint8_t  Time second
+ */
+uint8_t gps_time::get_second()
+{
+    return (_gps->time.second());
+}
+
+
+/**
+ * @brief Return GPS time centisecond
+ * 
+ * @return uint8_t  Time centisecond
+ */
+uint8_t gps_time::get_centisecond()
+{
+    return (_gps->time.centisecond());
 }
--- lib/gps_time/gps_time.h
+++ lib/gps_time/gps_time.h
@@ -2,18 +2,38 @@
 #define __GPS_TIME_T
 
 #include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
 #define GPS_UART_SPEED          ((uint32_t) 9600)
 
+
 class gps_time
 {
-private:
-    /* data */
-    void display_nfo();
+private: /* data */
+    TinyGPSPlus *_gps;
+    SoftwareSerial *_sserial;
+private: /* methods */
+    void display_info();
 public:
     gps_time(/* args */);
     void begin();
     void test();
+    //
+    void power(bool state);
+    void read_gps();
+    bool location_valid();
+    bool time_valid();
+    //
+    double get_lat();
+    double get_lng();
+    //
+    uint8_t get_month();
+    uint8_t get_day();
+    uint16_t get_year();
+    uint8_t get_hour();
+    uint8_t get_minute();
+    uint8_t get_second();
+    uint8_t get_centisecond();
 };
 
 
